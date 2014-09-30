92c13a42ca65cefaaacdf93b8e3eaaf220b32bdb
panmari <smmuzi@gmail.com>
2014-09-30 22:15:05 +0200

    reducing lap attributes into activity

--- activity.rb
+++ activity.rb
@@ -7,6 +7,25 @@ class Activity
     @laps = []
   end
 
+  def start_time
+    laps.first.start_time
+  end
+
+  def distance_meters
+    laps.reduce(0) { |sum, l| sum + l.distance_meters }
+  end
+
+  def maximum_speed
+    laps.reduce(0) { |max, l| [max, l.maximum_speed].max }
+  end
+
+  def total_time_seconds
+    laps.reduce(0) { |sum, l| sum + l.total_time_seconds }
+  end
+
+  def calories
+    laps.reduce(0) { |sum, l| sum + l.calories }
+  end
   def to_s
     "#{sport} " + laps.to_s
   end
--- tcx_parser_spec.rb
+++ tcx_parser_spec.rb
@@ -1,13 +1,16 @@
 require 'rspec'
 require_relative 'tcx_parser'
 
-describe 'Txc parser should be able to parse a simple file' do
+describe 'Txc parser should be able to parse a simple file with only one lap' do
 
   it 'should do something' do
     activities = TcxParser.parse('2012-08-23_Running.tcx')
     expect(activities.length).to be 1
     activity = activities.first
     expect(activity.sport).to eq 'Running'
-    puts activities
+    expect(activity.start_time).to eq activity.laps.first.start_time
+    expect(activity.distance_meters). to eq 8400.444984436035
+    expect(activity.maximum_speed). to eq 5.397222222222222
+    expect(activity.calories).to eq 596
   end
 end
